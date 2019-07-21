d129baac44a2f007cfe881e1521de2558f089873
Dennis Schwertel <dennisschwertel@gmail.com>
2019-07-21 11:29:52 +0200

    changes

--- add.go
+++ add.go
@@ -16,10 +16,20 @@ func check(e error) {
 		panic(e)
 	}
 }
-func main() {
 
-	var new_addon interface{}
-	var flavour interface{}
+type flavouraddonmeta struct {
+	Name string `yaml:"name"`
+}
+
+type flavouraddon struct {
+	Meta flavouraddonmeta `yaml:"meta"`
+}
+
+type flavourproject struct {
+	Addons yaml.MapSlice `yaml:"addons"`
+}
+
+func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 	var addon_yml []byte
@@ -34,11 +44,13 @@ func main() {
 	}
 
 	// read input go file for addon
+	new_addon := flavouraddon{}
 	err := yaml.Unmarshal([]byte(addon_yml), &new_addon)
 	check(err)
 	fmt.Printf("--- new addon ---:\n%v\n\n", new_addon)
 
 	// read flavour file
+	flavour := flavourproject{}
 	flavour_yml, err := ioutil.ReadFile("flavour.yml")
 	check(err)
 	err2 := yaml.Unmarshal([]byte(flavour_yml), &flavour)
@@ -47,30 +59,13 @@ func main() {
 	}
 	fmt.Printf("--- flavour ---:\n%v\n\n", flavour)
 
-	// -------------------------------------------------------------
-	// -------------------------------------------------------------
-	// -------------------------------------------------------------
-
-	new_addon_interface := new_addon.(map[interface{}]interface{})
-	addon_meta := new_addon_interface["meta"]
-	addon_meta_interface := addon_meta.(map[interface{}]interface{})
-	addon_name := addon_meta_interface["name"]
-
 	new_thing := make(map[string]interface{})
 	new_thing["manager"] = os.Getenv("FAM_IDENTIFIER")
 
-	m := flavour.(map[interface{}]interface{})
-	// TODO
-	n := m["addons"].(map[interface{}]interface{})
-	n[addon_name] = new_thing
-	// -------------------------------------------------------------
-	// -------------------------------------------------------------
-	// -------------------------------------------------------------
+	flavour.Addons = append(flavour.Addons, yaml.MapItem{new_addon.Meta.Name, new_thing})
 
 	s, err := yaml.Marshal(&flavour)
 	errs := ioutil.WriteFile("flavour_new.yml", s, 0644)
 	check(errs)
-
-	//TODO: write string s changes back to flavour.yml
 
 }
